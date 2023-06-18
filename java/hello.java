944d67c96b3979ab3699c87736c4b80e00b3c02c
gyu0918 <kjk1526@naver.com>
2023-06-18 23:26:57 +0900

    javajava

---
+++ java/hello.java
@@ -0,0 +1,5 @@
+public class hello{
+    public static void main(String[] args){
+        System.out.println("Hello!!");
+    }
+}<iframe width="1440" height="900" src="https://www.youtube.com/embed/8v_mjFMZ8XY" title="중거리 슛 138..? 콜라로프의 미친 ZD | 뿌챔스" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
