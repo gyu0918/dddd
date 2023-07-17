e57afda61837ca0c129161755bbe355c778b8823
gyu0918 <kjk1526@naver.com>
2023-07-17 20:59:21 +0900

    springBoot2

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+      //HashMap은 동시성 문제가 있기 때문에 컨커런트 헤시멥을 사용해야한다 실무에서!
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
