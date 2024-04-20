177ac51c28a41bb615e4fff7b863de6f02dd5ea8
gyu0918 <kjk1526@naver.com>
2024-04-20 14:27:39 +0900

    userDeletePractice

---
+++ userDelete/src/main/java/practice/demo/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package practice.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements userService{
+}
---
+++ userDelete/src/main/java/practice/demo/userService.java
@@ -0,0 +1,6 @@
+package practice.demo;
+
+
+public interface userService {
+
+}
