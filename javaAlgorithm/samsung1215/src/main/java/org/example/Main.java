31a449d33a4e06151b2dadcf6c5779fdaaa0987b
김정규 <111268088+gyu0918@users.noreply.github.com>
2024-05-08 10:42:57 +0900

    samsungAlgo

--- javaAlgorithm/samsung1215/src/main/java/org/example/Main.java
+++ javaAlgorithm/samsung1215/src/main/java/org/example/Main.java
@@ -5,14 +5,16 @@ package org.example;
 import java.util.*;
 
 public class Main {
-    private static int length;
+    private static int num;
     private static char[][] strArr;
 
     private static int count;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        length = sc.nextInt();
+        boolean flag = true;
+
+        num = sc.nextInt();
         strArr = new char[8][8];
         for(int i = 0; i < strArr.length; i++){
             String str = sc.next();
@@ -20,5 +22,27 @@ public class Main {
                 strArr[i][j] = str.charAt(j);
             }
         }
+
+        for (int i = 0; i < strArr.length; i++){
+            for (int j = 0; j < strArr.length - num + 1; j++){
+                //가로  회문 찾기
+                flag = true;
+                for(int k = 0; k < num / 2; k++){
+                    if (strArr[i][j + k] != strArr[i][j - k + num + 1])
+                        flag = false;
+                }
+                if (flag) count++;
+
+                //세로 회문 찾기
+                flag = true;
+                for(int k = 0; k < num/2; k++){
+                    if (strArr[j + k][i] != strArr[j - k + num + 1][i])
+                        flag = false;
+                }
+                if (flag) count++;
+            }
+        }
+        System.out.println("#" + + " " + count);
+
     }
 }
