b29f11477550d2856e8ec6e30f4e877800dd790a
김정규 <111268088+gyu0918@users.noreply.github.com>
2024-06-01 23:07:26 +0900

    webSocketPracitce2

--- webSocketPractice/src/main/java/com/example/demo/controller/ChatController.java
+++ webSocketPractice/src/main/java/com/example/demo/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ChatMessage;
 import com.example.demo.dto.ChatRoom;
+import com.example.demo.repository.ChatRoomRepository;
+import com.example.demo.service.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -28,16 +30,44 @@ import java.util.List;
 //    }
 //}
 
+
+/**
+ * before applying redis
+ */
+//@RequiredArgsConstructor
+//@RestController
+//public class ChatController {
+//
+//    private final SimpMessageSendingOperations messagingTemplate;
+//
+//    @MessageMapping("/chat/message")
+//    public void message(ChatMessage message) {
+//        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+//            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+//        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+//    }
+//}
+
+/**
+ * applying redis version
+ */
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
--- webSocketPractice/src/main/java/com/example/demo/dto/ChatRoom.java
+++ webSocketPractice/src/main/java/com/example/demo/dto/ChatRoom.java
@@ -6,25 +6,51 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * applying redis
+ */
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
 
-    public static ChatRoom create(String name){
+    public static ChatRoom create(String name) {
         ChatRoom chatRoom = new ChatRoom();
         chatRoom.roomId = UUID.randomUUID().toString();
         chatRoom.name = name;
         return chatRoom;
     }
+}
 
+/**
+ * before applying redis
+ */
+//@Getter
+//@Setter
+//public class ChatRoom {
+//    private String roomId;
+//    private String name;
+//
+//    public static ChatRoom create(String name){
+//        ChatRoom chatRoom = new ChatRoom();
+//        chatRoom.roomId = UUID.randomUUID().toString();
+//        chatRoom.name = name;
+//        return chatRoom;
+//    }
+//}
 
-    //<stomp로 구현전>
+/**
+ *  <stomp로 구현전>
+ */
 //    private Set<WebSocketSession> sessions = new HashSet<>();
 //
 //    @Builder
@@ -44,7 +70,7 @@ public class ChatRoom {
 //    public <T> void sendMessage(T message, ChatService chatService) {
 //        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
 //    }
-}
+
 
 
 ////채팅방 구현위한 dto
